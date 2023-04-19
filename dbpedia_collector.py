77a6ea2eba7e34c745682fab6d14b3512c9eb168
MR8u <maximerenard19@protonmail.com>
2023-04-19 16:36:14 +0200

    tests + changes to utils

--- dbpedia_collector.py
+++ dbpedia_collector.py
@@ -6,29 +6,6 @@ from pathlib import Path
 MANIFEST_PATH = Path('data/manifest.jsonl')
 BIOS_PATH = Path('data/bios/')
 
-ASTRAL_DATES = {
-    'Aries' : {'start':(3,21), 'end':(4,20)},
-    'Taurus' : {'start':(4,21), 'end':(5,20)},
-    'Gemini' : {'start':(5,21), 'end':(6,21)},
-    'Cancer' : {'start':(6,22), 'end':(7,22)},
-    'Leo' : {'start':(7,23), 'end':(8,22)},
-    'Virgo' : {'start':(8,23), 'end':(9,22)},
-    'Libra' : {'start':(9,23), 'end':(10,22)},
-    'Scorpio' : {'start':(10,23), 'end':(11,22)},
-    'Sagittarius' : {'start':(11,23), 'end':(12,21)},
-    'Capricorn' : {'start':(12,22), 'end':(1,20)},
-    'Aquarius' : {'start':(1,21), 'end':(2,19)},
-    'Pisces' : {'start':(2,20), 'end':(3,20)},
-}
-
-def date_to_astral(date, astral_dates=ASTRAL_DATES):
-    _, m, d = date.split('-')
-    m, d = int(m), int(d)
-    for k, v in astral_dates.items():
-        s, e = v.values()
-        if (m == s[0] and d > s[1]) or (m == e[0] and d < e[1]):
-            return k
-
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 sparql.setReturnFormat(JSON)
 
