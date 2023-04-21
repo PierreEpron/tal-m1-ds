d5685f9fc08486a95b7f0384ddb5fb0168b1eb0e
Pierre Epron <pierre.epron@gmail.com>
2023-04-21 16:10:10 +0200

    update file structure

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,17 +1,16 @@
 def clean_spacy_tokens(
-    txt,
-    tokens,
+    doc_json,
     stop_words=[], 
-    stop_pos=['ADV','PRON','CCONJ','PUNCT','PART','DET','ADP','SPACE', 'NUM'],
+    stop_pos=['ADV','PRON','CCONJ','PUNCT','PART','DET','ADP','SPACE','NUM','SYM','X'],
     min_size=2,
     lowercase=True,
     keep_lemma=True
 ):
     valid_tokens = []
 
-    for tok in tokens:
+    for tok in doc_json['tokens']:
 
-        tok_txt = txt[tok['start']:tok['end']]
+        tok_txt = doc_json['text'][tok['start']:tok['end']]
         tok_txt = tok_txt.lower() if lowercase else tok_txt
 
         if len(tok_txt) <= min_size:
@@ -25,20 +24,21 @@ def clean_spacy_tokens(
            
         valid_tokens.append(tok['lemma'] if keep_lemma else tok_txt)
 
-    return " ".join(valid_tokens)   
+    return valid_tokens
 
-from tqdm import tqdm
-from src.utils import read_jsonl, write_jsonl
-import spacy
+if __name__ == '__main__':
+    from tqdm import tqdm
+    from src.utils import read_jsonl, write_jsonl
+    import spacy
 
-MODEL_NAME = 'en_core_web_lg'
-INPUT_PATH = 'data/manifest.jsonl'
-OUTUT_PATH = f'data/{MODEL_NAME}.jsonl'
+    MODEL_NAME = 'en_core_web_lg'
+    INPUT_PATH = 'data/manifest.jsonl'
+    OUTUT_PATH = f'data/{MODEL_NAME}.jsonl'
 
-nlp = spacy.load(MODEL_NAME)
-docs = read_jsonl(INPUT_PATH)
+    nlp = spacy.load(MODEL_NAME)
+    docs = read_jsonl(INPUT_PATH)
 
-for doc in tqdm(docs):
-    doc['abstract'] = nlp(doc['abstract']).to_json()
+    for doc in tqdm(docs):
+        doc['abstract'] = nlp(doc['abstract']).to_json()
 
-write_jsonl(OUTUT_PATH, docs)
+    write_jsonl(OUTUT_PATH, docs)
