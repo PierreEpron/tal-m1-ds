85c4d1e919a2d64733488ca9fd0d7688d7df06dc
Pierre Epron <pierre.epron@gmail.com>
2023-05-14 19:57:32 +0200

    move astral into preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,52 +2,86 @@ from nltk.corpus import stopwords
 import nltk
 nltk.download('stopwords')
 
+ASTRAL_DATES = {
+    'Aries' : {'start':(3,21), 'end':(4,20)},
+    'Taurus' : {'start':(4,21), 'end':(5,20)},
+    'Gemini' : {'start':(5,21), 'end':(6,21)},
+    'Cancer' : {'start':(6,22), 'end':(7,22)},
+    'Leo' : {'start':(7,23), 'end':(8,22)},
+    'Virgo' : {'start':(8,23), 'end':(9,22)},
+    'Libra' : {'start':(9,23), 'end':(10,22)},
+    'Scorpio' : {'start':(10,23), 'end':(11,22)},
+    'Sagittarius' : {'start':(11,23), 'end':(12,21)},
+    'Capricorn' : {'start':(12,22), 'end':(1,20)},
+    'Aquarius' : {'start':(1,21), 'end':(2,19)},
+    'Pisces' : {'start':(2,20), 'end':(3,20)},
+}
+
+MONTHS = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+
+def date_to_astral(date, astral_dates=ASTRAL_DATES):
+    _, m, d = date.split('-')
+    m, d = int(m), int(d)
+    for k, v in astral_dates.items():
+        s, e = v.values()
+        if (m == s[0] and d >= s[1]) or (m == e[0] and d <= e[1]):
+            return k
 
 def clean_spacy_tokens(
-    doc_json,
     stop_words=stopwords.words('english'), 
     stop_pos=['ADV','PRON','CCONJ','PUNCT','PART','DET','ADP','SPACE','NUM','SYM','X'],
     min_size=2,
     lowercase=True,
     is_alpha=True,
-    keep_lemma=True
-):
-    valid_tokens = []
+    keep_lemma=True,
+    remove_month=True # Only work if lowercase == True:
+    ):
 
-    for tok in doc_json['tokens']:
+    if remove_month:
+        stop_words += MONTHS
 
-        tok_txt = doc_json['text'][tok['start']:tok['end']]
-        tok_txt = tok_txt.lower() if lowercase else tok_txt
+    def wrapped_clean_spacy_tokens(
+        doc_json
+    ):
+        valid_tokens = []
 
-        if is_alpha and not tok_txt.isalpha():
-            continue
+        for tok in doc_json['tokens']:
 
-        if len(tok_txt) <= min_size:
-           continue
+            tok_txt = doc_json['text'][tok['start']:tok['end']]
+            tok_txt = tok_txt.lower() if lowercase else tok_txt
 
-        if tok['pos'] in stop_pos:
-           continue
+            if is_alpha and not tok_txt.isalpha():
+                continue
 
-        if tok_txt in stop_words:
-           continue
-           
-        valid_tokens.append(tok['lemma'] if keep_lemma else tok_txt)
+            if len(tok_txt) <= min_size:
+                continue
 
-    return valid_tokens
+            if tok['pos'] in stop_pos:
+                continue
 
-if __name__ == '__main__':
-    from tqdm import tqdm
-    from src.utils import read_jsonl, write_jsonl
-    import spacy
+            if tok_txt in stop_words:
+                continue
 
-    MODEL_NAME = 'en_core_web_lg'
-    INPUT_PATH = 'data/manifest.jsonl'
-    OUTUT_PATH = f'data/{MODEL_NAME}.jsonl'
+            valid_tokens.append(tok['lemma'] if keep_lemma else tok_txt)
 
-    nlp = spacy.load(MODEL_NAME)
-    docs = read_jsonl(INPUT_PATH)
+        return valid_tokens
+    
+    return wrapped_clean_spacy_tokens
 
-    for doc in tqdm(docs):
-        doc['abstract'] = nlp(doc['abstract']).to_json()
 
-    write_jsonl(OUTUT_PATH, docs)
+# if __name__ == '__main__':
+#     from tqdm import tqdm
+#     from src.utils import read_jsonl, write_jsonl
+#     import spacy
+
+#     MODEL_NAME = 'en_core_web_lg'
+#     INPUT_PATH = 'data/manifest.jsonl'
+#     OUTUT_PATH = f'data/{MODEL_NAME}.jsonl'
+
+#     nlp = spacy.load(MODEL_NAME)
+#     docs = read_jsonl(INPUT_PATH)
+
+#     for doc in tqdm(docs):
+#         doc['abstract'] = nlp(doc['abstract']).to_json()
+
+#     write_jsonl(OUTUT_PATH, docs)
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,6 @@
 from pathlib import Path
 import json
 
-ASTRAL_DATES = {
-    'Aries' : {'start':(3,21), 'end':(4,20)},
-    'Taurus' : {'start':(4,21), 'end':(5,20)},
-    'Gemini' : {'start':(5,21), 'end':(6,21)},
-    'Cancer' : {'start':(6,22), 'end':(7,22)},
-    'Leo' : {'start':(7,23), 'end':(8,22)},
-    'Virgo' : {'start':(8,23), 'end':(9,22)},
-    'Libra' : {'start':(9,23), 'end':(10,22)},
-    'Scorpio' : {'start':(10,23), 'end':(11,22)},
-    'Sagittarius' : {'start':(11,23), 'end':(12,21)},
-    'Capricorn' : {'start':(12,22), 'end':(1,20)},
-    'Aquarius' : {'start':(1,21), 'end':(2,19)},
-    'Pisces' : {'start':(2,20), 'end':(3,20)},
-}
-
 def read_jsonl(path, encoding='utf-8'):
     """
         Shortcut for read jsonl file
@@ -40,11 +25,3 @@ def write_jsonl(path, data, encoding='utf-8'):
     """
     path = Path(path) if isinstance(path, str) else path
     path.write_text('\n'.join([json.dumps(item) for item in data]), encoding=encoding)
-
-def date_to_astral(date, astral_dates=ASTRAL_DATES):
-    _, m, d = date.split('-')
-    m, d = int(m), int(d)
-    for k, v in astral_dates.items():
-        s, e = v.values()
-        if (m == s[0] and d >= s[1]) or (m == e[0] and d <= e[1]):
-            return k
